084c81786b36ab86e029cc0815cc5898e245213c
Arthurlenzi <arthurlenzi@gmail.com>
2021-02-22 11:19:06 -0300

    Updating Model

--- engine/Model.php
+++ engine/Model.php
@@ -4,36 +4,53 @@ namespace Engine;
 
 use lib\PDOHelper;
 use lib\Validator;
+use lib\CustomException;
 
 abstract class Model
 {
     protected $databases;
-    protected $prefix;
     protected $config;
     protected $env_state;
     protected $container;
     protected $validator;
     private $error_message;
+    protected $dir;
+    protected $config_dir;
 
-    public function __construct($state, $config, $container)
+    public function __construct()
     {
-        $this->states = parse_ini_file('..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'state.ini', true);
-        $this->databases = parse_ini_file('..' . DIRECTORY_SEPARATOR . 'config'
-                                               . DIRECTORY_SEPARATOR . 'database.ini', true);
-        $this->prefix = $state;
-        $this->config = $config;
-        $this->container = $container;
+        $this->dir = str_replace("engine", "", __DIR__);
+        $this->config_dir = $this->dir . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+        $this->databases = parse_ini_file($this->config_dir . 'database.ini', true);
+        $this->config = parse_ini_file($this->config_dir . 'config.ini', true);
+        $this->container = array();
         $this->validator = new Validator();
+        $this->setEnvState();
+    }
 
-        $this->env_state = array_search($this->prefix, $this->states);
+    public function setEnvState()
+    {
+        $this->states = parse_ini_file($this->dir . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'state.ini', true);
+        $request_uri = 'REQUEST_URI';
+        $develop_str = 'develop';
+        if (strpos($_SERVER[$request_uri], $this->states['homolog']) !== false) {
+            $urlEnvParameter = 'homolog';
+        } elseif (strpos($_SERVER[$request_uri], $this->states[$develop_str]) !== false) {
+            $urlEnvParameter = $develop_str;
+        } elseif (strpos($_SERVER[$request_uri], $this->states['training']) !== false) {
+            $urlEnvParameter = $develop_str;
+        } else {
+            $urlEnvParameter = 'default';
+        }
+        $this->env_state = array_search($urlEnvParameter, $this->states);
         if (!$this->env_state) {
-            $this->env_state = $this->prefix; # Expected to be default
+            $this->env_state = $urlEnvParameter; # Expected to be default
         }
     }
 
     public function getServerApi()
     {
-        $availableApis = parse_ini_file('..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'api.ini', true);
+        $availableApis = parse_ini_file($this->dir . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'api.ini', true);
         $apiInfo = $availableApis[$this->env_state];
         $api = $apiInfo['host'];
         if ($apiInfo['port']) {
@@ -51,7 +68,7 @@ abstract class Model
      * @param string url
      * @param array parameters (with keys)
      * @param array headers (without keys)
-     * @return array [data, response code]
+     * @return array [data, http_code, header_size, result]
      */
     public function curlGET($url, $parameters, $headers = array())
     {
@@ -76,12 +93,19 @@ abstract class Model
         $curl  = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER, true);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
         $result = curl_exec($curl);
+
+        // Busca dados da requisição curl
         $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $body = substr($result, $header_size);
+        
         curl_close($curl);
 
-        return array("data" => $result, "http_code" => $httpcode);
+        return array("data" => $body, "http_code" => $httpcode, "header_size" => $header_size, 'result' => $result);
     }
 
     /**
@@ -135,9 +159,24 @@ abstract class Model
         return $filtered;
     }
 
+    // Cria array de bindings através de um array associativo ($chave => $valor)
+    public function createBindingsArray($parameters)
+    {
+        $bindingsArray = array();
+        foreach ($parameters as $key => $value) {
+            $bindingsArray[':' . $key] = $this->setValor($parameters, $key);
+        }
+
+        return $bindingsArray;
+    }
+
     public function setValor($dados, $key)
     {
-        return (isset($dados[$key]) && $dados[$key]) ? $dados[$key] : null;
+        if (isset($dados[$key]) && $dados[$key] === false) {
+            $dados[$key] = "false";
+            return $dados[$key];
+        }
+        return isset($dados[$key]) ? $dados[$key] : null;
     }
 
     public function setValorCheckbox($dados, $key)
@@ -151,8 +190,7 @@ abstract class Model
         $rules = array('date' => 'date_format:Y-m-d');
         if (!$this->validator->validate($data, $rules)['valid']) {
             $this->handleError(1, $this->error_message);
-        }
-        else {
+        } else {
             $dt = new \DateTime();
             $date = $dt->createFromFormat('Y-m-d', $date);
             return $date->format('Y-m-d');
@@ -166,7 +204,6 @@ abstract class Model
             $this->handleError(1, $this->error_message);
         }
         return $this->returnDate($dados[$key]);
-        
     }
 
     public function setTimeStamp($dados, $key)
@@ -205,6 +242,20 @@ abstract class Model
         }
     }
 
+    public function begin($db)
+    {
+        $this->container[$db]->beginTransaction();
+    }
+    public function commit($db)
+    {
+        $this->container[$db]->commit();
+    }
+    public function rollBack($db)
+    {
+        $this->container[$db]->rollBack();
+    }
+
+
     public function openConnect($database)
     {
         $db_data = $this->databases[$this->env_state . '_' . $database];
@@ -215,5 +266,35 @@ abstract class Model
         $pass = $db_data['password'];
 
         return new PDOHelper($dsn, $user, $pass, $this->env_state, $db_data['type'], $this->config, []);
+    }
+
+    /**
+     * Retorna o IP do Cliente.
+     *
+     * @return void
+     */
+    public function getIP()
+    {
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ip;
+    }
+    
+    /**
+     * Formata um array para se adequar a clausula IN
+     *
+     * @param mixed $arr
+     *
+     * @return [type]
+     */
+    public function createInClause($arr)
+    {
+        return '\'' . implode('\', \'', $arr) . '\'';
     }
 }
